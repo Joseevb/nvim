2b5f1a60f94364e1a5ac7d273ba0d8b3d96d0bd3
Jose Vasquez <joseevasquezbarboza@gmail.com>
2024-10-01 16:08:51 +0200

    fixed lomobok

--- lua/jose/plugins/jdtls.lua
+++ lua/jose/plugins/jdtls.lua
@@ -1,28 +1,24 @@
+-- To make java work with lomobok, this must be added to .bashrc or .zshrc
+-- export JDTLS_JVM_ARGS="-javaagent:$HOME/.local/share/nvim/mason/share/jdtls/lombok.jar"
+
 return {
     "mfussenegger/nvim-jdtls",
     config = function()
         local jdtls = require("jdtls")
 
+        -- Helper function to get the jdtls configuration
         local function get_config()
             local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
             local workspace_dir = "/home/jose/.cache/jdtls/workspace/jdt.ls-java-project/" .. project_name
 
+            -- Path to google-java-format jar
+            local home = os.getenv('HOME')
+            local google_java_format_jar = home .. '/workspace/bin/google-java-format-1.23.0-all-deps.jar'
+
             return {
                 cmd = {
-                    "java",
-                    "-javaagent:/home/jose/.local/share/nvim/mason/share/jdtls/lombok.jar",
-                    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-                    "-Dosgi.bundles.defaultStartLevel=4",
-                    "-Declipse.product=org.eclipse.jdt.ls.core.product",
-                    "-Dlog.protocol=true",
-                    "-Dlog.level=ALL",
-                    "-Xms1g",
-                    "--add-modules=ALL-SYSTEM",
-                    "--add-opens", "java.base/java.util=ALL-UNNAMED",
-                    "--add-opens", "java.base/java.lang=ALL-UNNAMED",
-                    "-jar", "/home/jose/.local/share/nvim/mason/share/jdtls/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar",
-                    "-configuration", "/home/jose/.local/share/nvim/mason/share/jdtls/config",
-                    "-data", workspace_dir,
+                    "jdtls",
+                    "--jvm-arg=" .. string.format("-javaagent:%s", vim.fn.expand "$MASON/share/jdtls/lombok.jar"),
                 },
                 root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" }),
                 settings = {
@@ -31,13 +27,46 @@ return {
                             Lombok = {
                                 enabled = true,
                             }
-                        }
+                        },
+                        format = {
+                            enabled = true,
+                            settings = {
+                                url = google_java_format_jar,
+                            },
+                        },
                     },
                 },
                 handlers = {
                     ["language/status"] = function(_, result) end,
                     ["$/progress"] = function(_, result, ctx) end,
                 },
+                on_attach = function(client, bufnr)
+                    -- Enable formatting on save only for Java files
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        callback = function()
+                            -- Only format if the file type is Java
+                            if vim.bo.filetype == 'java' and vim.api.nvim_buf_is_loaded(bufnr) then
+                                vim.lsp.buf.format({ timeout_ms = 1000 })  -- Use `format` instead of `formatting_sync`
+                            end
+                        end,
+                    })
+                    -- 
+                    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
+
+                    -- Automatically add imports on paste
+                    vim.api.nvim_create_autocmd("TextYankPost", {
+                        buffer = bufnr,
+                        callback = function()
+                            -- Trigger code action to add missing imports after pasting
+                            vim.lsp.buf.code_action({
+                                context = { only = { "source.addImports" } },  -- Limit the code action to adding imports
+                                apply = true,
+                            })
+                        end,
+                    })
+                end
+
             }
         end
 
@@ -50,4 +79,3 @@ return {
         })
     end,
 }
-
